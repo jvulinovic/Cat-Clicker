614ce89c3741ee8d8bd2541bac57ce6765514ef8
Jadran Vulinovic <jvulinovic@gmail.com>
2018-12-09 20:19:41 +1100

    adding second cat

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
 
 // declaring variables
 
-let catClicks = 0;
+let catClicksOne = 0;
+let catImageOne = document.getElementById('cat1');
+let clickCounterOne = document.getElementById('clickCounter1');
+let catName1 = document.getElementById('catName1');
+let name1 = "Anna";
 
-let catImage = document.getElementById('cat');
-
-let clickCounter = document.getElementById('clickCounter');
-
+let catClicksTwo = 0;
+let catImageTwo = document.getElementById('cat2');
+let clickCounterTwo = document.getElementById('clickCounter2');
+let catName2 = document.getElementById('catName2');
+let name2 = "Gabby";
 
 // function to count the clicks
 
-catImage.addEventListener('click', function(){
-  catClicks ++;
-  clickCounter.innerHTML = "Number of Clicks = " + catClicks;
-}, false);
+function catNames () {
+  catName1.innerHTML = name1;
+  catName2.innerHTML = name2;
+};
+
+function catCounterOne () {
+  catImageOne.addEventListener('click', function() {
+    catClicksOne ++;
+    clickCounterOne.innerHTML = "Number of Clicks = " + catClicksOne;
+  }, false);
+};
+
+function catCounterTwo () {
+  catImageTwo.addEventListener('click', function() {
+    catClicksTwo ++;
+    clickCounterTwo.innerHTML = "Number of Clicks = " + catClicksTwo;
+  }, false);
+};
+
+catCounterOne();
+catCounterTwo();
+catNames();
