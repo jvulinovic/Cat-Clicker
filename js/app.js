c8c5b014479ade36fdb90cebf18c26cd9342e536
Jadran Vulinovic <jvulinovic@gmail.com>
2018-12-12 14:01:25 +1100

    included click counter for each cat

--- js/app.js
+++ js/app.js
@@ -1,44 +1,6 @@
 
 // declaring variables
 
-let catClicksOne = 0;
-let catImageOne = document.getElementById('cat1');
-let clickCounterOne = document.getElementById('clickCounter1');
-let catName1 = document.getElementById('catName1');
-let name1 = "Anna";
-
-let catClicksTwo = 0;
-let catImageTwo = document.getElementById('cat2');
-let clickCounterTwo = document.getElementById('clickCounter2');
-let catName2 = document.getElementById('catName2');
-let name2 = "Gabby";
-
-// function to count the clicks
-
-function catNames () {
-  catName1.innerHTML = name1;
-  catName2.innerHTML = name2;
-};
-
-function catCounterOne () {
-  catImageOne.addEventListener('click', function() {
-    catClicksOne ++;
-    clickCounterOne.innerHTML = "Number of Clicks = " + catClicksOne;
-  }, false);
-};
-
-function catCounterTwo () {
-  catImageTwo.addEventListener('click', function() {
-    catClicksTwo ++;
-    clickCounterTwo.innerHTML = "Number of Clicks = " + catClicksTwo;
-  }, false);
-};
-
-// catCounterOne();
-// catCounterTwo();
-// catNames();
-
-
 let catnames = ["Fred", "Harry", "Jim", "Josie", "Fatso"];
 
 let catimages = ["catone.jpg", "cattwo.jpg", "catthree.jpg", "catfour.jpg", "catfive.jpg"];
@@ -49,20 +11,27 @@ let cattitle = document.getElementById('cattitle');
 
 let catimage = document.getElementById('catimage');
 
+let clickCounter = document.getElementById('clickCounter');
+
+
 function createList () {
   for (const [index, value] of catnames.entries()) {
     var elem = document.createElement('div');
     elem.textContent = value;
     elem.className = "catlistitem";
-    elem.addEventListener('click', (function(numCopy) {
+    elem.addEventListener('click', (function(catname) {
         return function() {
-            cattitle.innerHTML = numCopy;
+            let catClicks = 0;
+            cattitle.innerHTML = catname;
             catimage.src = "img/" + catimages[index];
+            catimage.addEventListener('click', function () {
+              catClicks ++;
+              clickCounter.innerHTML = "Number of Clicks = " + catClicks;
+            }, false);
           };
         })(value));
         container.appendChild(elem);
       };
 };
-
 
 createList ();
